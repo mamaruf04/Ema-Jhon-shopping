db3307c49f6a72d3301cdcb2e613eb9e81fc1c5c
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-03-26 12:49:58 +0600

    added product component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import '../Shop/Shop.css'
+import Product from './Product/Product';
+
+
+const Shop = () => {
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
+
+    const handelAddToCard = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product=><Product
+                         key={product.id}
+                         Product={product}
+                         handelAddToCard = {handelAddToCard}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
